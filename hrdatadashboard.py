70485413fb09e292974a890719b98044d9c2c24a
YOONJEUN <001.t3st@gmail.com>
2026-08-06 17:05:46 +0900

    처음 테스트

--- hrdatadashboard.py
+++ hrdatadashboard.py
@@ -5,4 +5,9 @@ import matplotlib.pyplot as plt
 import koreanize_matplotlib
 
 df = pd.read_csv("./HR Data.csv")
+df.head(10)
 
+
+total_employees = len(df)
+total_attritions = df['퇴직'].sum()
+overall_rate = round(df['퇴직'].mean() * 100 , 1)
